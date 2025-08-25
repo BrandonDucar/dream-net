4f71328a956a38fde79b86a616409b049dd5c263
BDucar <brandonducar123@gmail.com>
2025-08-24 21:36:19 -0700

    Update main.tsx to include AdminOverlay

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,12 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import AdminOverlay from "./components/AdminOverlay";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <AdminOverlay />
+  </React.StrictMode>
+);
