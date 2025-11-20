b43917195ba25603e6707ebee953fd819cb6c70e
BDucar <brandonducar123@gmail.com>
2025-11-19 19:24:14 -0500

    Trigger redeploy: Add comment to main entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// DreamNet Hub - Main entry point
 createRoot(document.getElementById("root")!).render(<App />);
