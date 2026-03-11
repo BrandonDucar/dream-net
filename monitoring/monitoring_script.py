799efb84a4f73274458b5fc7387e6c7cfcd0abb6
Brandon Ducar <brandonducar1234@gmail.com>
2026-03-11 12:30:52 -0400

    [LilMissClaw] Enhanced monitoring script to check statuses and log events.

--- monitoring/monitoring_script.py
+++ monitoring/monitoring_script.py
@@ -2,15 +2,24 @@
 import time
 import requests
 
-def check_email_status():
-    # Logic to check email sending status
-    pass
+class MonitoringSystem:
+    def check_email_status(self):
+        # Logic to check the status of sent emails
+        response = requests.get('/email/status_endpoint')  # Example endpoint
+        return response.json()
 
-def check_sms_status():
-    # Logic to check SMS sending status
-    pass
+    def check_sms_status(self):
+        # Logic to check status of sent SMS
+        response = requests.get('/sms/status_endpoint')  # Example endpoint
+        return response.json()
 
+    def log_event(self, event_info):
+        # Log or process the event information from the webhook
+        print(f'Event logged: {event_info}')
+
+monitoring_system = MonitoringSystem()
 while True:
-    check_email_status()
-    check_sms_status()
+    email_status = monitoring_system.check_email_status()
+    sms_status = monitoring_system.check_sms_status()
+    monitoring_system.log_event({'email_status': email_status, 'sms_status': sms_status})
     time.sleep(60)  # Check every minute
